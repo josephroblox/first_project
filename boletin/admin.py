ac94ff973b4500a8a3454b125f2a8cc21c5a2452
Joseph <josephroblox2002@gmail.com>
2023-02-08 15:40:57 -0800

    Update Admin Registrado

--- boletin/admin.py
+++ boletin/admin.py
@@ -3,11 +3,11 @@ from .forms import RegModelForm
 from boletin.models import Registrado
 
 class AdminRegistrado(admin.ModelAdmin):
-    list_display = ["id", "email", "nombre", "timestamp"] #aqui se escribe las columnas que quiero q tenga la tabla
+    list_display = ["id", "email", "nombre"] #aqui se escribe las columnas que quiero q tenga la tabla
     form = RegModelForm
     list_filter = ["timestamp", "nombre"]#aqui se escriben los filtros que quiero seleccionar
     search_fields =["nombre", "email"]#aqui se escriben los campos de busqueda escrita
-    list_editable =["nombre", "email"]#me sirvio para modificar esas propiedas en la tabla
+    #list_editable =["nombre", "email"]#me sirvio para modificar esas propiedas en la tabla
    #class Meta:
     #model = Registrado 
     
